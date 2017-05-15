fd35dbadac90e29625d45a838c2eae91dc98784b
Aman Pratap Singh <aps10@iitbbs.ac.in>
2017-05-15 08:10:00 +0000

    Solution Accepted

---
+++ hackerrank/duplication.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int duplication(int x){
+    // Complete this function
+    int i = 0;
+    while (x != 0) {
+	    i = i + x%2;
+	    x = x/2;
+    }
+    i = i%2;
+    return i;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int q;
+    cin >> q;
+    for(int a0 = 0; a0 < q; a0++){
+        int x;
+        cin >> x;
+        int result = duplication(x);
+        cout << result << endl;
+    }
+    return 0;
+}

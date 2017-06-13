bb979903820dbe3df716b9f3b9bb8f1d7472d121
Aman Pratap Singh <aps10@iitbbs.ac.in>
2017-06-13 14:57:06 +0530

    Solution Accepted

--- codechef/zco12001.cpp
+++ codechef/zco12001.cpp
@@ -1,41 +1,74 @@
 /**
-*	Aman Pratap Singh (@apsknight)
-*	Indian Institute of Technology Bhubaneswar
-*	www.apsknight.cf
+*	Aman Pratap Singh (@apsknight, </www.apsknight.cf>)
+*	Indian Institute of Technology Bhubaneswar 
 */
-
 #include <bits/stdc++.h>
 using namespace std;
 
 typedef long long ll;
+typedef string st;
+typedef vector<int> vi;
+typedef vector<st> vs;
 #define rep(i, n) for(int i = 0; i < n; i++)
-#define test int t; cin >> t; while(t--)
-#define fori(i,a,b) for(int i = (a); i <= (b); i++)
+#define fogg(i,a,b) for(int i = (a); i <= (b); i++)
 #define ford(i,a,b) for(int i = (a); i >= (b); i--)
+#define test int t; cin >> t; while(t--)
+#define input ll a[10000000]; ll n; cin >> n; rep(i, n) cin >> a[i]
 #define debug(x) cout << '>' << #x << ':' << x << "\n";
+#define print(x) cout << x << "\n"
+#define printsl(x) cout << x << " "
 #define off ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define MOD 1e9+7
 
 int main() {
 	off;
-	int n;
+	int n, nd = 0, ni = 0, mx = 0, mi = 0;
+	
 	cin >> n;
-	int a[100005] = {0};
+
+	int a[n];
 
 	rep(i, n) {
 		cin >> a[i];
 	}
-	int mx = 0, t = 0;
-	fori(i, 1, n) {
-		if(a[i] = 1 && a[i-1] == 1) {
-			t++;
-		}
-		if (a[i] == 2)
-			t = 0;
 
-		mx = max(mx, t);
+	int temp = 0;
+	rep(i, n) {
+		if (a[i] == 1) {
+			temp++;
+		}
+		else {
+			if (temp > nd) {
+				nd = temp;
+				ni = i;
+			}
+			temp--;
+		}
 	}
 
-	debug(mx);
+	int pent = 0, tem = 0;
+	rep(i, n) {	
+		if (a[i] == 1) {
+			pent++;
+		}
+
+		else {
+			pent--;
+		}
+		if (pent != 0) {
+				tem++;
+		}
+		else if (pent == 0) {
+			if (tem > mx) {
+				mx = tem + 1;
+				mi = i - mx + 2;
+			}
+			tem = 0;
+		}
+		// debug(pent);
+	}
+
+	cout << nd << " " << ni << " " << mx << " " << mi << "\n";
 
   	return 0;
 }

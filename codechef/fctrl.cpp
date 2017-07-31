b7eeb25e0f636f0c64c5081380f71055d7044679
Aman Pratap Singh <aps10@iitbbs.ac.in>
2017-07-31 21:28:11 +0530

    Problem Solved

---
+++ codechef/fctrl.cpp
@@ -0,0 +1,37 @@
+/**
+*	Aman Pratap Singh (@apsknight, </www.amanpratapsingh.in>)
+*	Indian Institute of Technology Bhubaneswar 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef string st;
+typedef vector<int> vi;
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define fogg(i,a,b) for(int i = (a); i <= (b); ++i)
+#define ford(i,a,b) for(int i = (a); i >= (b); --i)
+#define test int t; cin >> t; while(t--)
+#define debug(x) cout << '>' << #x << ':' << x << "\n";
+#define endl '\n'
+#define off ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define mxx 1e6+7
+
+int main() {
+	off;
+	test {
+		ll n;
+		cin >> n;
+
+		int res = 0, temp = 1;
+
+		while (n / pow(5, temp) > 0) {
+			res += n / pow(5, temp);
+			temp++;
+		}
+
+		cout << res << endl;
+	}
+
+  	return 0;
+}
---
+++ codechef/hs08test.cpp
@@ -0,0 +1,35 @@
+/**
+*	Aman Pratap Singh (@apsknight, </www.amanpratapsingh.in>)
+*	Indian Institute of Technology Bhubaneswar 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef string st;
+typedef vector<int> vi;
+#define rep(i, n) for(int i = 0; i < n; ++i)
+#define fogg(i,a,b) for(int i = (a); i <= (b); ++i)
+#define ford(i,a,b) for(int i = (a); i >= (b); --i)
+#define test int t; cin >> t; while(t--)
+#define debug(x) cout << '>' << #x << ':' << x << "\n";
+#define endl '\n'
+#define off ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define mxx 1e6+7
+
+int main() {
+	off;
+	ll x;
+	double y;
+
+	cin >> x >> y;
+
+	if (x % 5 == 0 && x < (ll) y) {
+		cout << fixed << setprecision(2) << y - x - 0.50 << endl; 
+	}
+	else {
+		cout << fixed << setprecision(2) << y << endl;
+	}
+
+  	return 0;
+}
